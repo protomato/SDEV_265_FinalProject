f3a51ade09aed3b632a4fbb5c859a7dc09751541
Jazer Vargas <74873418+jazerjos@users.noreply.github.com>
2023-10-29 12:01:09 -0400

    Add files via upload

--- Board.py
+++ Board.py
@@ -1,18 +1,9 @@
-from tkinter import Tk, Canvas, Frame, Button
+from tkinter import Tk, Canvas, Frame, Label ,Button
 from PIL import Image, ImageTk
-import pygame
-import random
 from MiniGame_One import MiniGame_One 
 from MiniGame_Two import MiniGame_Two 
-
-#newstuff
-pos=0
-boardsize=24
-qsize=6
-
-pygame.init()
-root = Tk()
-root.title("Board")
+from Player import *
+import random
 
 # Constants for window size and grid setup
 ROWS,COLS = 7,7 #delimite how many squares you want to have
@@ -23,30 +14,12 @@ SQUARE_SIZE = 100  # the size of the square is width divided by number of rows
 SQUARE_SIZE_X = WIDTH // COLS 
 SQUARE_SIZE_Y = WIDTH // COLS 
 
-# Image path and positions for player pieces
-
-image_path_Two = "images\playerTwo.png"
-original_image_Two = Image.open(image_path_Two)
-resized_image2 = original_image_Two.resize((SQUARE_SIZE-50, SQUARE_SIZE-50))		   
-image_Two = ImageTk.PhotoImage(resized_image2)
-
-image_path_Three = "images\playerThree.png"
-original_image_Three = Image.open(image_path_Three)
-resized_image_Three = original_image_Three.resize((SQUARE_SIZE-50, SQUARE_SIZE-50))		   
-image_Three = ImageTk.PhotoImage(resized_image_Three)
-
-image_path_Four = "images\playerFour.png"
-original_image_Four = Image.open(image_path_Four)
-resized_image_Four = original_image_Four.resize((SQUARE_SIZE-50, SQUARE_SIZE-50))		   
-image_Four = ImageTk.PhotoImage(resized_image_Four)
-
-player_positions = [(0, 0), (0, 6), (6, 6), (6, 0)] # list containing multiple tuples each tuple represent the position of the player on the board
-
 # Define color constants
 RED = (255,0,0)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 BLUE = (0,0,255)
+
 
 class EndTurnButton(Button):
 	def __init__(self,root,player_positions,my_button_move):
@@ -57,19 +30,20 @@ class EndTurnButton(Button):
 	def on_button_click(self):
 		self.grid_remove()
 		self.my_button_move.grid(row=ROWS // 2, column=COLS // 2)
-		
+		board.labelPlayer(board.get_current_player_index())
+
 class PlayButton(Button):
 	def __init__(self, root,player_positions):
 		# Call the constructor of the base class (Button)
 		super().__init__(root, text="Play ", command=self.on_button_click,width=10,height=3)
 		self.gameWon = False
-		self.button_move = MyButtonMove(root,player_positions,False,text="Move")
+		self.button_move = ButtonMove(root,player_positions,False,text="Move")
 		self.button_move.grid(row=ROWS//2, column=COLS//2)
 		self.player_positions=player_positions
 		self.endTurn = False
 		self.row=0
 		self.col=0
-
+		
 	def notMove(self,row,colum):
 		self.row=row
 		self.col=colum
@@ -119,7 +93,7 @@ class PlayButton(Button):
 			self.grid_remove()
 			self.button_move.getEndButton().grid(row=ROWS//2, column=COLS//2)
 			board.swap_current_four_players()
-
+			
 	def MiniGame_One(self):	
 		#check if the player won or lost the mini-game
 		game_one = MiniGame_One()	
@@ -132,29 +106,31 @@ class PlayButton(Button):
 			self.grid_remove()
 			self.button_move.getEndButton().grid(row=ROWS//2, column=COLS//2)
 			board.swap_current_four_players()
-			print("current player "+str(board.get_current_player_index()))
+			#print("current player "+str(board.get_current_player_index()))
 
 	def getendTurn(self):
 		return self.endTurn
 
-class MyButtonMove(Button):
+class ButtonMove(Button):
 	def __init__(self, root,player_positions,endTurn,text="Play "):
 		# Call the constructor of the base class (Button)
 		super().__init__(root, text=text, command=self.on_button_click,width=10,height=3)
 		self.player_positions = player_positions
 		self.endTurn = endTurn
-		#self.button_move = MyButtonMove(root,player_positions,False,text="Move")
 		self.button_End = EndTurnButton(root,player_positions,self)
 
 	def getEndButton(self):
 		return self.button_End 
 
 	def on_button_click(self):
+		pos = 0
+		boardsize=24
+		qsize=6
 		roll=random.randrange(1,6)
 		#roll=1   
-		global pos, boardsize, qsize # Add player_positions to the list of global variables
+		#global pos, boardsize, qsize # Add player_positions to the list of global variables
 		pos=(pos+roll)%boardsize
-		print(pos)
+		#print(pos)
 		fliped=(pos>=(qsize*2))
 		temp=pos%(qsize*2)
 		if fliped:
@@ -195,16 +171,36 @@ class MyButtonMove(Button):
 			else:
 				self.player_positions[3] = [(fi,si)]  #player_four position update
 				board.move_player(fi,si,4)
-				print(self.player_positions)
+				#print(self.player_positions)
 
 class Board:
-	def __init__(self,player_positions):
+	def __init__(self,root):
+		self.root=root
+		# list containing multiple tuples each tuple represent the position of the player on the board
+		self.player_positions = [(0, 0), (0, 6), (6, 6), (6, 0)] 
+		
 		self.image_path = "images\playerOne.png"
 		self.original_image = Image.open(self.image_path)
 		self.resized_image = self.original_image.resize((SQUARE_SIZE-50, SQUARE_SIZE-50))
 		self.image = ImageTk.PhotoImage(self.resized_image)
+
+		self.image_path_Two = "images\playerTwo.png"
+		self.original_image_Two = Image.open(self.image_path_Two)
+		self.resized_image2 = self.original_image_Two.resize((SQUARE_SIZE-50, SQUARE_SIZE-50))		   
+		self.image_Two = ImageTk.PhotoImage(self.resized_image2)
+
+		self.image_path_Three = "images\playerThree.png"
+		self.original_image_Three = Image.open(self.image_path_Three)
+		self.resized_image_Three = self.original_image_Three.resize((SQUARE_SIZE-50, SQUARE_SIZE-50))		   
+		self.image_Three = ImageTk.PhotoImage(self.resized_image_Three)
+
+		self.image_path_Four = "images\playerFour.png"
+		self.original_image_Four = Image.open(self.image_path_Four)
+		self.resized_image_Four = self.original_image_Four.resize((SQUARE_SIZE-50, SQUARE_SIZE-50))		   
+		self.image_Four = ImageTk.PhotoImage(self.resized_image_Four)
+		
 		self.board = [] # Initialize an empty list to represent the game board.
-		self.player_positions = player_positions 	
+		self.player_positions = self.player_positions 	
 		self.red_left = self.white_left = 12 
 		self.squares = [[None for _ in range(COLS)] for _ in range(ROWS)]
 		self.create_board(0,0)
@@ -214,10 +210,13 @@ class Board:
 		self.player_row_Two, self.player_col_Two = 0, 6
 		self.player_row_Three, self.player_col_Three = 6, 6
 		self.player_row_Four, self.player_col_Four = 6, 0
-		root.bind("<Key>", self.on_key_press)
+		#root.bind("<Key>", self.on_key_press)
 		self.gameWon = False
-		self.buttonPlay=PlayButton(root,self.player_positions)
+		self.buttonPlay= PlayButton(root,self.player_positions)
 		self.current_player_index = 1
+		self.label = Label(root, text="Turn Player 1", bg="#D3D3D3",padx=10, pady=10)
+		self.labelTwo = Label(root, text="Blue", bg="#D3D3D3",padx=10, pady=10)
+		self.labelPlayer(self.current_player_index)
 
 	def get_current_player_index(self):
 		return self.current_player_index
@@ -227,7 +226,7 @@ class Board:
 		self.current_player_index = 2 if self.current_player_index == 1 else 1
 
 	def swap_current_four_players(self):
-		if self.current_player_index == 4:
+		if  self.current_player_index == 4:
 			self.current_player_index = 4 if self.current_player_index == 1 else 1
 		elif self.current_player_index == 3:
 			self.current_player_index = 3 if self.current_player_index == 4 else 4
@@ -237,10 +236,11 @@ class Board:
 			self.current_player_index = 1 if self.current_player_index == 2 else 2
 		else :
 			print ("Not a player")
+		return self.current_player_index
 
 	def getPlayButton(self):
 		return self.buttonPlay
-
+		
 	def on_key_press(self, event):
 		x_coordinate = self.player_positions[0][1]
 		y_coordinate = self.player_positions[0][0]
@@ -253,6 +253,7 @@ class Board:
 			x_coordinate += 1
 		if event.keysym == "a":
 			x_coordinate -= 1
+		
 
 	# Create a new tuple with the updated coordinates
 		self.player_positions = ((y_coordinate, x_coordinate))
@@ -261,7 +262,19 @@ class Board:
 		self.player_row = x_coordinate
 		self.player_col = y_coordinate
 	
+			
+	def getPlayerColorTwo(self):
+		return self.image_Two
 
+	def getPlayerColorThree(self):
+		return self.image_Three
+	
+	def getPlayerColorFour(self):
+		return self.image_Four
+
+	def GetBoard(self):
+		return self
+	
 	def draw_squares(self):
 		for rows in range(ROWS):
 			for col in range (COLS):
@@ -284,11 +297,10 @@ class Board:
 			self.board.append([])
 			for col in range(COLS):
 				if (row, col) in self.player_positions:
-					#print(str(row)+" "+str(col))
-					self.board[row].append(Player(row, col,self.image))
-					self.board[row].append(Player(row, col,image_Two))
-					self.board[row].append(Player(row, col,image_Three))
-					self.board[row].append(Player(row, col,image_Four))
+					self.board[row].append(Player(row, col,self.image,self))
+					self.board[row].append(Player(row, col,self.image_Two,self))
+					self.board[row].append(Player(row, col,self.image_Three,self))
+					self.board[row].append(Player(row, col,self.image_Four,self))
 				else:
 					self.board[row].append(0)
 		
@@ -344,27 +356,48 @@ class Board:
 				rect = self.squares[row][col]
 				rect.config(bg=colorBg)
 				rect.delete("all")
+				
 				#player 1
 				canvas_widget=self.squares[self.player_row_One][self.player_col_One]#control movement player one
 				canvas_widget.create_image(50, 50, anchor="nw", image=self.image)
-				
+	
 				#player 2
 				rect = self.squares[0][6]
 				canvas_widget=self.squares[self.player_row_Two][self.player_col_Two]    #control movement player 2
-				canvas_widget.create_image(0, 50, anchor="nw", image=image_Two)
-				
+				canvas_widget.create_image(0, 50, anchor="nw", image=self.image_Two)
+
 				#player 3
 				rect = self.squares[row][col]
 				rect = self.squares[6][6]
 				canvas_widget=self.squares[self.player_row_Three][self.player_col_Three]    #control movement player 3
-				canvas_widget.create_image(0, 0, anchor="nw", image=image_Three)
+				canvas_widget.create_image(0, 0, anchor="nw", image=self.image_Three)
 	
 				#player 4
 				rect = self.squares[row][col]
 				rect = self.squares[6][0]
 				canvas_widget=self.squares[self.player_row_Four][self.player_col_Four]    #control movement player 4
-				canvas_widget.create_image(50, 0, anchor="nw", image=image_Four)
-
+				canvas_widget.create_image(50, 0, anchor="nw", image=self.image_Four)
+				#self.labelPlayer(self.current_player_index)
+	
+	def labelPlayer(self,currentPlayer):
+			if currentPlayer==1:
+				self.label.grid(row=1, column=1,columnspan=2,sticky='nw')
+				self.labelTwo.grid(row=1, column=1,columnspan=2,sticky='sw')
+				self.labelTwo.config(text="Blue")
+			elif currentPlayer==2:
+				self.label.grid(row=1, column=5,columnspan=2,sticky='nw')
+				self.labelTwo.grid(row=1, column=5,columnspan=2,sticky='sw')
+				self.labelTwo.config(text="Green")
+			elif currentPlayer==3:
+				self.label.grid(row=5, column=5,columnspan=2,sticky='nw')
+				self.labelTwo.grid(row=5, column=5,columnspan=2,sticky='sw')
+				self.labelTwo.config(text="Red")
+			elif currentPlayer==4:
+				self.label.grid(row=5, column=1,columnspan=2,sticky='nw')
+				self.labelTwo.grid(row=5, column=1,columnspan=2,sticky='sw')
+				self.labelTwo.config(text="Yellow")
+			self.label.config(text="Turn Player "+str(self.current_player_index))
+	#def playerDisplay(self,canvas,):
 	def draw(self):
 		for self.player_row in range(ROWS):
 			for self.player_col in range(COLS):
@@ -382,43 +415,13 @@ class Board:
 		y_coordinate = col	
 		self.buttonPlay.notMove(x_coordinate,y_coordinate)
 		self.buttonPlay.grid(row=ROWS//2, column=COLS//2,columnspan=1)
-
-class Player:
-	def __init__(self,row,col,image):			   
-		self.image = image
-		self.row = row
-		self.col = col
-		
-		#function position 
-	def position(self):
-		self.x =self.col   # x position 
-		self.y =self.row   #y position
-		
-
-	def draw(self,rect,player):  #load the image on the board on the giving position by the function position
-		if player==1:
-			rect.create_image(50, 50, anchor="nw", image=self.image)
-		if player==2:
-			rect.create_image(0, 50, anchor="nw", image=image_Two)
-		if player==3:
-			rect.create_image(0, 0, anchor="nw", image=image_Three)
-		if player==4:
-			rect.create_image(50, 0, anchor="nw", image=image_Four)			
-	
-info = pygame.display.Info()
-
-# Get screen width and height
-screen_width = info.current_w
-screen_height = info.current_h
-
-# Calculate the center of the screen
-center_x = screen_width // 2
-center_y = screen_height // 2
-
-board = Board(player_positions)
-
+			
+root = Tk()
+board = Board(root)	
 def play():
-		pygame.init()
-		root.mainloop()
+	root.title("Board")
+	root.winfo_toplevel().positionfrom("user")
+	root.mainloop()	
+	return board
 
-#play()
+
